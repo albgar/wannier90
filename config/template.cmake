e672b9f61db73abdf0395773b1c1c49fa861b1a4
Alberto Garcia <albertog@icmab.es>
2022-11-18 15:55:20 +0100

    Add basic CMake framework

---
+++ config/template.cmake
@@ -0,0 +1,8 @@
+@PACKAGE_INIT@
+
+if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
+  include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
+
+  include(CMakeFindDependencyMacro)
+
+endif()
